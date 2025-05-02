0d5919265260500cb570720056ff1befac24f4c7
ElenaKr77 <elena.kriventsova77@gmail.com>
2025-05-02 23:53:48 +0300

    change src

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long expected = 30;
+        long actual = service.calculate(1000, true);
+        System.out.println(actual);
+
+        expected = 500;
+        actual = service.calculate(1000000, true);
+        System.out.println(actual);
+
+        expected = 500;
+        actual = service.calculate(100000, true);
+        System.out.println(actual);
+    }
+}
